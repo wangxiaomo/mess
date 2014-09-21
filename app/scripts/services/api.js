fadcfe660718dc3a7b40b27457195c6464a18065
wangxiaomo <wxm4ever@gmail.com>
2014-09-21 20:35:32 +0800

    补全api

--- app/scripts/services/api.js
+++ app/scripts/services/api.js
@@ -29,7 +29,11 @@ angular.module('gollumApp')
       '简历宝典': 'news/jlbd',
       '面试心经': 'news/msxj',
       '跳槽攻略': 'news/tcgl',
-      '办公人际': 'news/bgrj'
+      '办公人际': 'news/bgrj',
+      '职场测试': 'news/zccs',
+      '福利政策': 'news/flzc',
+      '公益活动': 'news/gyhd',
+      '网站公告': 'news/wzgg'
     };
     this.getNew = function(category) {
       return getAPIData(this.CategoryMap[category]);
