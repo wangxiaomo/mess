908612c10b8b6dd8ca165b7543cdb2248bc1a0cb
wangxiaomo <wxm4ever@gmail.com>
2014-10-08 11:13:19 +0800

    去除tips

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ define('SITE_PATH', getcwd() . '/');
 //项目路径
 define('PROJECT_PATH', SITE_PATH . 'shuipf/');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG', false);
+define('APP_DEBUG', true);
 // 应用公共目录
 define('COMMON_PATH', PROJECT_PATH . 'Common/');
 // 定义应用目录
