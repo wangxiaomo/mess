70978190ed2b031c619cf26f63ac671ae03838c3
wangxiaomo <wxm4ever@gmail.com>
2014-10-09 11:16:22 +0800

    add css and js to header

--- shuipf/Application/Admin/View/Common/Cssjs.php
+++ shuipf/Application/Admin/View/Common/Cssjs.php
@@ -1,3 +1,7 @@
 <?php if (!defined('SHUIPF_VERSION')) exit(); ?><link href="{$config_siteurl}statics/css/admin_style.css" rel="stylesheet" />
 <link href="{$config_siteurl}statics/js/artDialog/skins/default.css" rel="stylesheet" />
+<link href="{$config_siteurl}statics/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+<link href="{$config_siteurl}statics/bower_components/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet" />
+<link href="{$config_siteurl}statics/bower_components/jquery-file-upload/css/jquery.fileupload.css" rel="stylesheet" />
+<link href="{$config_siteurl}statics/bower_components/jquery-file-upload/css/jquery.fileupload-ui.css" rel="stylesheet" />
 <Admintemplate file="Admin/Common/Js"/>
--- shuipf/Application/Admin/View/Common/Js.php
+++ shuipf/Application/Admin/View/Common/Js.php
@@ -7,4 +7,15 @@ var GV = {
 };
 </script>
 <script src="{$config_siteurl}statics/js/wind.js"></script>
-<script src="{$config_siteurl}statics/js/jquery.js"></script>
+<script src="{$config_siteurl}statics/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="{$config_siteurl}statics/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="{$config_siteurl}statics/bower_components/blueimp-canvas-to-blob/js/canvas-to-blob.js"></script>
+<script src="{$config_siteurl}statics/bower_components/blueimp-load-image/js/load-image.all.min.js"></script>
+<script src="{$config_siteurl}statics/bower_components/jquery.ui/ui/widget.js"></script>
+<script src="{$config_siteurl}statics/bower_components/jquery-file-upload/js/jquery.fileupload.js"></script>
+<script src="{$config_siteurl}statics/bower_components/jquery-file-upload/js/jquery.iframe-transport.js"></script>
+<script src="{$config_siteurl}statics/bower_components/jquery-file-upload/js/jquery.fileupload-process.js"></script>
+<script src="{$config_siteurl}statics/bower_components/jquery-file-upload/js/jquery.fileupload-audio.js"></script>
+<script src="{$config_siteurl}statics/bower_components/jquery-file-upload/js/jquery.fileupload-image.js"></script>
+<script src="{$config_siteurl}statics/bower_components/jquery-file-upload/js/jquery.fileupload-video.js"></script>
+<script src="{$config_siteurl}statics/bower_components/jquery-file-upload/js/jquery.fileupload-validate.js"></script>
