27f8660cd0b53ff497039ecfbc9b6e8ea4201d07
wangxiaomo <wxm4ever@gmail.com>
2014-10-09 15:57:06 +0800

    turn off debug mode

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ define('PROJECT_PATH', SITE_PATH . 'shuipf/');
 define('UTILS_PATH', SITE_PATH . 'utils/');
 define('UPLOAD_PATH', SITE_PATH . 'upload/');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG', true);
+define('APP_DEBUG', false);
 // 应用公共目录
 define('COMMON_PATH', PROJECT_PATH . 'Common/');
 // 定义应用目录
